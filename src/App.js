b37ba0075e45652840c7c421737db07714d0a745
Liiiziiiiii <119292426+Liiiziiiiii@users.noreply.github.com>
2023-11-02 19:35:14 +0200

    add 6 web lab

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './Components/Home';
+import Jewelry from './Components/Jewelry';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <Jewelry/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
