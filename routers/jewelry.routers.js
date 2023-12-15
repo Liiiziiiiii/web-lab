4495fcab955eec3aff7e670ce3a0c27aa2799f23
Liiiziiiiii <119292426+Liiiziiiiii@users.noreply.github.com>
2023-12-15 18:21:37 +0200

    Delete routers directory

--- routers/jewelry.routers.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express")
-
-const router = new Router()
-const jewelryController = require(`../controller/jewelry.controller`)
-
-router.get('/jewelry', jewelryController.getJewelry)
-
-module.exports = router
